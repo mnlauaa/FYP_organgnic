a4bbc5d9d2fb9dcb848f86a36e02272496eaf74c
TsuiHinCHi <hctsuiaa@connect.ust.hk>
2018-01-17 16:17:13 +0800

    add db tale news, shopping cart, product

--- sql/fyp_organgnic.sql
+++ sql/fyp_organgnic.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主機: localhost
--- 產生時間： 2018 年 01 月 16 日 10:20
+-- 產生時間： 2018 年 01 月 17 日 09:15
 -- 伺服器版本: 10.1.19-MariaDB
 -- PHP 版本： 5.6.28
 
@@ -66,6 +66,22 @@ CREATE TABLE `chat_logs` (
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `news`
+--
+
+CREATE TABLE `news` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `product_id` int(11) UNSIGNED DEFAULT NULL,
+  `seller_id` int(11) UNSIGNED DEFAULT NULL,
+  `datetime` datetime DEFAULT NULL,
+  `title` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `active` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `order_forms`
 --
 
@@ -75,6 +91,21 @@ CREATE TABLE `order_forms` (
   `seller_id` int(11) UNSIGNED DEFAULT NULL,
   `date` date DEFAULT NULL,
   `sataus` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `name` text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
+  `qty` int(11) DEFAULT NULL,
+  `seller_id` int(11) UNSIGNED DEFAULT NULL,
+  `image_url` varchar(500) CHARACTER SET utf8 DEFAULT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -114,6 +145,20 @@ CREATE TABLE `sellers` (
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `shopping_cart`
+--
+
+CREATE TABLE `shopping_cart` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `product_id` int(11) UNSIGNED DEFAULT NULL,
+  `buyer_id` int(11) UNSIGNED DEFAULT NULL,
+  `qty` int(11) UNSIGNED DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `transactions`
 --
 
@@ -142,9 +187,21 @@ ALTER TABLE `chat_logs`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `news`
+--
+ALTER TABLE `news`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 資料表索引 `order_forms`
 --
 ALTER TABLE `order_forms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `product`
+--
+ALTER TABLE `product`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -157,6 +214,12 @@ ALTER TABLE `products`
 -- 資料表索引 `sellers`
 --
 ALTER TABLE `sellers`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `shopping_cart`
+--
+ALTER TABLE `shopping_cart`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -180,14 +243,29 @@ ALTER TABLE `buyers`
 ALTER TABLE `chat_logs`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
+-- 使用資料表 AUTO_INCREMENT `news`
+--
+ALTER TABLE `news`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
 -- 使用資料表 AUTO_INCREMENT `order_forms`
 --
 ALTER TABLE `order_forms`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
+-- 使用資料表 AUTO_INCREMENT `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
 -- 使用資料表 AUTO_INCREMENT `sellers`
 --
 ALTER TABLE `sellers`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- 使用資料表 AUTO_INCREMENT `shopping_cart`
+--
+ALTER TABLE `shopping_cart`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- 使用資料表 AUTO_INCREMENT `transactions`
