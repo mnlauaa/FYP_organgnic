d455b0dc223fe6213bb65f8c1fd6454c23aa8947
Sam Lau <mnlauaa@connect.ust.hk>
2018-03-07 14:54:24 +0800

    change sql

--- sql/fyp_organgnic.sql
+++ sql/fyp_organgnic.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 02, 2018 at 01:01 PM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+-- 主機: 127.0.0.1
+-- 產生時間： 2018-03-07 07:53:45
+-- 伺服器版本: 10.1.30-MariaDB
+-- PHP 版本： 7.2.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `fyp_organgnic`
+-- 資料庫： `fyp_organgnic`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `chat_logs`
+-- 資料表結構 `chat_logs`
 --
 
 CREATE TABLE `chat_logs` (
@@ -38,7 +40,7 @@ CREATE TABLE `chat_logs` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `farms`
+-- 資料表結構 `farms`
 --
 
 CREATE TABLE `farms` (
@@ -50,7 +52,7 @@ CREATE TABLE `farms` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `farms`
+-- 資料表的匯出資料 `farms`
 --
 
 INSERT INTO `farms` (`id`, `seller_id`, `phone_number`, `address`, `active`) VALUES
@@ -60,7 +62,7 @@ INSERT INTO `farms` (`id`, `seller_id`, `phone_number`, `address`, `active`) VAL
 -- --------------------------------------------------------
 
 --
--- Table structure for table `favorite`
+-- 資料表結構 `favorite`
 --
 
 CREATE TABLE `favorite` (
@@ -73,7 +75,7 @@ CREATE TABLE `favorite` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `news`
+-- 資料表結構 `news`
 --
 
 CREATE TABLE `news` (
@@ -89,7 +91,7 @@ CREATE TABLE `news` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order_forms`
+-- 資料表結構 `order_forms`
 --
 
 CREATE TABLE `order_forms` (
@@ -97,14 +99,14 @@ CREATE TABLE `order_forms` (
   `farm_id` int(11) UNSIGNED DEFAULT NULL,
   `seller_id` int(11) UNSIGNED DEFAULT NULL,
   `date` date DEFAULT NULL,
-  `sataus` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `sataus` int(2) NOT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `products`
+-- 資料表結構 `products`
 --
 
 CREATE TABLE `products` (
@@ -121,7 +123,7 @@ CREATE TABLE `products` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `products`
+-- 資料表的匯出資料 `products`
 --
 
 INSERT INTO `products` (`id`, `farm_id`, `name`, `qty`, `price`, `weight`, `rating`, `rating_number`, `image_url`, `active`) VALUES
@@ -136,7 +138,7 @@ INSERT INTO `products` (`id`, `farm_id`, `name`, `qty`, `price`, `weight`, `rati
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reviews`
+-- 資料表結構 `reviews`
 --
 
 CREATE TABLE `reviews` (
@@ -151,7 +153,7 @@ CREATE TABLE `reviews` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `reviews`
+-- 資料表的匯出資料 `reviews`
 --
 
 INSERT INTO `reviews` (`id`, `farm_id`, `product_id`, `type`, `rating`, `comment`, `date`, `active`) VALUES
@@ -162,7 +164,7 @@ INSERT INTO `reviews` (`id`, `farm_id`, `product_id`, `type`, `rating`, `comment
 -- --------------------------------------------------------
 
 --
--- Table structure for table `shopping_cart`
+-- 資料表結構 `shopping_cart`
 --
 
 CREATE TABLE `shopping_cart` (
@@ -176,7 +178,7 @@ CREATE TABLE `shopping_cart` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transactions`
+-- 資料表結構 `transactions`
 --
 
 CREATE TABLE `transactions` (
@@ -191,7 +193,7 @@ CREATE TABLE `transactions` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- 資料表結構 `users`
 --
 
 CREATE TABLE `users` (
@@ -204,138 +206,149 @@ CREATE TABLE `users` (
   `address` varchar(500) CHARACTER SET utf8 DEFAULT NULL,
   `liabilities` tinyint(1) NOT NULL DEFAULT '0',
   `profile_pic_url` varchar(500) DEFAULT NULL,
-  `identity` varchar(100) NOT NULL,
+  `identity` int(1) NOT NULL,
   `iat` varchar(100) CHARACTER SET utf8 NOT NULL,
   `active` tinyint(1) UNSIGNED DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- 資料表的匯出資料 `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `fb_id`, `display_name`, `phone_number`, `address`, `liabilities`, `profile_pic_url`, `identity`, `iat`, `active`) VALUES
-(1, 'user1', NULL, NULL, 'seller1', NULL, NULL, 0, NULL, 'seller', '', 1),
-(2, 'user2', NULL, NULL, 'seller2', NULL, NULL, 0, NULL, 'seller', '', 1),
-(3, 'user3', NULL, NULL, 'buyer1', NULL, NULL, 0, NULL, 'buyer', '', 1);
+(1, 'user1', NULL, NULL, 'seller1', NULL, NULL, 0, NULL, 0, '', 1),
+(2, 'user2', NULL, NULL, 'seller2', NULL, NULL, 0, NULL, 0, '', 1),
+(3, 'user3', NULL, NULL, 'buyer1', NULL, NULL, 0, NULL, 0, '', 1);
 
 --
--- Indexes for dumped tables
+-- 已匯出資料表的索引
 --
 
 --
--- Indexes for table `chat_logs`
+-- 資料表索引 `chat_logs`
 --
 ALTER TABLE `chat_logs`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `farms`
+-- 資料表索引 `farms`
 --
 ALTER TABLE `farms`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `favorite`
+-- 資料表索引 `favorite`
 --
 ALTER TABLE `favorite`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `news`
+-- 資料表索引 `news`
 --
 ALTER TABLE `news`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `order_forms`
+-- 資料表索引 `order_forms`
 --
 ALTER TABLE `order_forms`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `products`
+-- 資料表索引 `products`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `reviews`
+-- 資料表索引 `reviews`
 --
 ALTER TABLE `reviews`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `shopping_cart`
+-- 資料表索引 `shopping_cart`
 --
 ALTER TABLE `shopping_cart`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `transactions`
+-- 資料表索引 `transactions`
 --
 ALTER TABLE `transactions`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- 資料表索引 `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- 在匯出的資料表使用 AUTO_INCREMENT
 --
 
 --
--- AUTO_INCREMENT for table `chat_logs`
+-- 使用資料表 AUTO_INCREMENT `chat_logs`
 --
 ALTER TABLE `chat_logs`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `farms`
+-- 使用資料表 AUTO_INCREMENT `farms`
 --
 ALTER TABLE `farms`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
 --
--- AUTO_INCREMENT for table `favorite`
+-- 使用資料表 AUTO_INCREMENT `favorite`
 --
 ALTER TABLE `favorite`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `news`
+-- 使用資料表 AUTO_INCREMENT `news`
 --
 ALTER TABLE `news`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `order_forms`
+-- 使用資料表 AUTO_INCREMENT `order_forms`
 --
 ALTER TABLE `order_forms`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `products`
+-- 使用資料表 AUTO_INCREMENT `products`
 --
 ALTER TABLE `products`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
--- AUTO_INCREMENT for table `reviews`
+-- 使用資料表 AUTO_INCREMENT `reviews`
 --
 ALTER TABLE `reviews`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 --
--- AUTO_INCREMENT for table `shopping_cart`
+-- 使用資料表 AUTO_INCREMENT `shopping_cart`
 --
 ALTER TABLE `shopping_cart`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `transactions`
+-- 使用資料表 AUTO_INCREMENT `transactions`
 --
 ALTER TABLE `transactions`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
 --
--- AUTO_INCREMENT for table `users`
+-- 使用資料表 AUTO_INCREMENT `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
