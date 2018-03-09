79b7a300885deb213eb6e90b3cc115b3c8de1c06
Apple <grace12wong@gmail.com>
2018-03-09 15:45:47 +0800

    add news controller and model

--- app/controllers/news.js
+++ app/controllers/news.js
@@ -8,15 +8,20 @@ module.exports = {
 }
 
 async function getNewsById(ctx) {
-
+    let id = ctx.params.id;
+    let news = await newsModel.findNewsById(id);
+    ctx.body = news[0];
 }
 
 async function getNewsList(ctx) {
-    
+    let news = await newsModel.findAllNews();
+	ctx.body = news;
 }
 
 async function postNews(ctx) {
-    
+    let datetime = new datetime();
+    let input = [ctx.params.id, ctx.params.farm_id, datetime, ctx.params.title, ctx.params.description, ctx.params.image_url];
+    let news = await newsModel.postNews(input);
 }
 
 async function putNews(ctx) {
--- app/models/newsModel.js
+++ app/models/newsModel.js
@@ -1,6 +1,24 @@
 const db = require('../utils/database')
 
 const news = {
+    async findNewsById(id) {
+		let _sql = 'SELECT * FROM news WHERE id = ?';
+		let news = await db.query(_sql, id);
+		return news;
+    },
+    
+    async findAllNews() {
+		let _sql = 'SELECT * FROM news';	
+		let news = await db.query(_sql);
+		return news;
+    },
+
+    async postNews(input) {
+        let _sql = 'INSERT INTO news (id, farm_id, datetime, title, description, image_url) VALUES (?)';	
+		let news = await db.query(_sql, [input]);
+		return news;
+    },
+
 }
 
 module.exports = news
